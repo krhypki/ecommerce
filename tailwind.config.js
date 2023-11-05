2114c4017e06788acd884bd381f3341385c77e5e
Krzysztof Hypki <krhypki@gmail.com>
2023-11-05 13:30:57 +0100

    Init

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    colors: {
+      primary: '495E57',
+      light: '#F5F7F8'
+    }
+  },
+  plugins: [],
+}
