30b001fe5631efe201ab224ef0ae655458ca5ac7
Krzysztof Hypki <krhypki@gmail.com>
2023-11-05 17:17:47 +0100

    Navbar

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  app: {
+    head: {
+      link: [
+        {
+          rel: 'stylesheet',
+          href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css'
+        }
+      ]
+    }
+  },
   modules: ['@nuxtjs/tailwindcss'],
-  css: ['~/assets/scss/style.scss']
+  css: ['~/assets/scss/style.scss'],
+  components: [
+    {
+      path: '~/components',
+      pathPrefix: false
+    }
+  ]
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [],
+  mode: 'jit',
   theme: {
     colors: {
-      primary: '495E57',
-      light: '#F5F7F8'
+      primary: '#495E57',
+      light: '#F5F7F8',
+      black: '#000'
     }
-  },
-  plugins: [],
+  }
 }
